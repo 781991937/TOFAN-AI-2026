e25ce3b5895ece2959f89d0e63274bc09c980c9d
RAED TOFAN <raedtofan7@gmail.com>
2026-09-20 20:30:27 +0300

    agent: export conversation memory

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -2,14 +2,10 @@
 
 from .models import Agent, AgentKind, AgentRun, AgentStatus, AgentTool
 from .service import AgentContext, AgentError, AgentService
+from .memory import AgentConversation, AgentMessageRecord
 
 __all__ = [
-    "Agent",
-    "AgentContext",
-    "AgentError",
-    "AgentKind",
-    "AgentRun",
-    "AgentService",
-    "AgentStatus",
-    "AgentTool",
+    "Agent", "AgentContext", "AgentError", "AgentKind", "AgentRun",
+    "AgentService", "AgentStatus", "AgentTool",
+    "AgentConversation", "AgentMessageRecord",
 ]
