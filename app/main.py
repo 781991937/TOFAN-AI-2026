15449d0eb5dafbdc9481b4657fbc2daeccff6b17
RAED TOFAN <raedtofan7@gmail.com>
2026-09-20 20:15:30 +0300

    api: register academy administration routes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI
 
 from app.api.admin import router as admin_router
+from app.api.academy_admin import router as academy_admin_router
 from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 
@@ -10,6 +11,7 @@ app = FastAPI(title="TOFAN Smart Academy", version="0.1.0")
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(admin_router)
+app.include_router(academy_admin_router)
 
 
 def main() -> None:
