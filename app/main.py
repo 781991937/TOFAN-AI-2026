66002455f0f133dc520135204a3db49d9492974e
RAED TOFAN <raedtofan7@gmail.com>
2026-09-22 20:24:35 +0300

    feat: register student assessment API

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ from app.api.main_manager import router as main_manager_router
 from app.api.notifications import router as notifications_router
 from app.api.learning_progress import router as learning_progress_router
 from app.api.certificates import router as certificates_router
+from app.api.student_assessments import router as student_assessments_router
 from app.api.manager_dashboard import router as manager_dashboard_router
 from app.db.init_db import init_db
 from app.db.session import SessionLocal
@@ -79,6 +80,7 @@ app.include_router(main_manager_router)
 app.include_router(notifications_router)
 app.include_router(learning_progress_router)
 app.include_router(certificates_router)
+app.include_router(student_assessments_router)
 app.include_router(manager_dashboard_router)
 app.include_router(curriculum_router)
 app.include_router(specialty_experience_router)
